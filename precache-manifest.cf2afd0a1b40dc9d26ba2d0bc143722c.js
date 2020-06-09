241e44799f1f7cb473dcd107b820b6e538bed687
Bratishques <54228461+Bratishques@users.noreply.github.com>
2020-06-09 21:21:58 +0300

    Updates

---
+++ precache-manifest.cf2afd0a1b40dc9d26ba2d0bc143722c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c3402c7ab89512bd4b431125262a94",
+    "url": "/generic-shop/index.html"
+  },
+  {
+    "revision": "d3fcbd141d8b1528528a",
+    "url": "/generic-shop/static/css/main.30993a25.chunk.css"
+  },
+  {
+    "revision": "a57fa5503e5ee5cc5770",
+    "url": "/generic-shop/static/js/2.dd916b8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/generic-shop/static/js/2.dd916b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3fcbd141d8b1528528a",
+    "url": "/generic-shop/static/js/main.b619f196.chunk.js"
+  },
+  {
+    "revision": "044133f0d1ccd92fcaff",
+    "url": "/generic-shop/static/js/runtime-main.ff045ff1.js"
+  },
+  {
+    "revision": "1108481815f75313eb0a5c9f6b239886",
+    "url": "/generic-shop/static/media/bath.11084818.png"
+  },
+  {
+    "revision": "8af0064d4dfd26afd222b5c225c4419b",
+    "url": "/generic-shop/static/media/gel.8af0064d.png"
+  },
+  {
+    "revision": "2537f65833d383906198df6bc6082e5b",
+    "url": "/generic-shop/static/media/minus.2537f658.svg"
+  },
+  {
+    "revision": "fb8228d267c9fea0f2caf83ba6c412b6",
+    "url": "/generic-shop/static/media/plus.fb8228d2.svg"
+  },
+  {
+    "revision": "a4b4fc5c514594ba4fe1ec687a8ae98f",
+    "url": "/generic-shop/static/media/soap.a4b4fc5c.png"
+  },
+  {
+    "revision": "97bae268f16d56875b0973a20d88618e",
+    "url": "/generic-shop/static/media/trash.97bae268.svg"
+  },
+  {
+    "revision": "d3c19c456e9181f5368042adad6560f3",
+    "url": "/generic-shop/static/media/whiteminus.d3c19c45.svg"
+  },
+  {
+    "revision": "67d33532acdd3b5e399fb9054703780a",
+    "url": "/generic-shop/static/media/whiteplus.67d33532.svg"
+  }
+]);
